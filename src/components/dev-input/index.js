4216b6a81f3718e3b0426edae5a9c3960338c6a2
Pablo Tedesco <tedesco8@hotmail.com>
2022-08-27 17:07:07 -0300

    custom validation added for dev-input

--- src/components/dev-input/index.js
+++ src/components/dev-input/index.js
@@ -1,30 +1,68 @@
-import React from "react";
-import { Input } from "./style";
+import React from 'react';
+import {
+  InputGroup,
+  Input,
+  Label,
+  FeedbackError,
+  IconValidation,
+} from './style';
+
+import {
+  faCheckCircle,
+  faTimesCircle,
+} from "@fortawesome/free-solid-svg-icons";
 
 const DevInput = ({
   id,
   type,
   name,
-  value,
-  onClick,
-  onChange,
-  required,
   placeholder,
+  color,
+  state,
+  regex,
+  errorMsg,
+  nonLabel,
+  handleChange,
   children,
 }) => {
+  const onChange = (e) => {
+    handleChange({ ...state, value: e.target.value });
+  };
+
+  const validation = () => {
+    if (regex) {
+      if (regex.test(state.value)) {
+        handleChange({ ...state, isValid: 'true' });
+        return;
+      }
+      handleChange({ ...state, isValid: 'false' });
+    }
+  };
   return (
-    <Input
-      id={id}
-      type={type}
-      name={name}
-      value={value}
-      placeholder={placeholder}
-      required={required}
-      onClick={onClick}
-      onChange={onChange}
-    >
-      {children}
-    </Input>
+    <>
+      {!nonLabel && <Label htmlFor={id}>{name}:</Label>}
+      <InputGroup>
+        <Input
+          id={id}
+          type={type}
+          name={name}
+          placeholder={placeholder}
+          color={color}
+          value={state.value}
+          isValid={state.isValid}
+          onBlur={validation}
+          onKeyUp={validation}
+          onChange={onChange}
+        >
+          {children}
+        </Input>
+        <IconValidation
+          icon={state.isValid === 'true' ? faCheckCircle : faTimesCircle}
+          isValid={state.isValid}
+        />
+      </InputGroup>
+      <FeedbackError isValid={state.isValid}>{errorMsg}</FeedbackError>
+    </>
   );
 };
 
--- src/components/dev-input/style.js
+++ src/components/dev-input/style.js
@@ -1,10 +1,18 @@
-import styled from "styled-components";
+import styled, { css } from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const InputGroup = styled.div`
+  position: relative;
+  @media (max-width: 768px) {
+    padding-bottom: 5px !important;
+  }
+`;
 
 const Input = styled.input`
   max-width: 100%;
   padding: 11px 13px;
   background: #f9f9fa;
-  color: var(--black2);
+  color: ${(props) => (props.color ? props.color : 'var(--black2)')};
   margin-bottom: 0.9rem;
   border-radius: 10px;
   outline: 0;
@@ -16,14 +24,74 @@ const Input = styled.input`
   :hover {
     box-shadow: 0 0 3px rgba(0, 0, 0, 0.15), 0 1px 5px rgba(0, 0, 0, 0.1);
   }
-  :focus,
-  :invalid:required {
-    border: 1px solid red;
-  }
-  :focus,
-  :valid {
-    border: 1px solid green;
+
+  ${(props) =>
+    props.isValid === 'true' &&
+    css`
+      border: 1px solid hsl(112, 66%, 52%);
+    `}
+
+  ${(props) =>
+    props.isValid === 'false' &&
+    css`
+      border: 1px solid hsl(351, 98%, 49%) !important;
+    `}
+
+    ${(props) =>
+    props.inline &&
+    css`
+      border-radius: 10px 0 0 10px !important;
+    `}
+`;
+
+const Label = styled.label`
+  font-size: 19px;
+  text-transform: uppercase;
+  font-weight: 600;
+  width: 100%;
+  display: block;
+  @media (max-width: 768px) {
+    font-size: 10pt;
   }
 `;
 
-export { Input };
+const IconValidation = styled(FontAwesomeIcon)`
+  position: absolute;
+  right: 10px;
+  bottom: 14px;
+  z-index: 1;
+  font-size: 16px;
+  color: hsl(112, 66%, 52%);
+  opacity: 0;
+  ${(props) =>
+    props.isValid === 'true' &&
+    css`
+      opacity: 1;
+      color: hsl(112, 66%, 52%);
+    `}
+
+  ${(props) =>
+    props.isValid === 'false' &&
+    css`
+      opacity: 1;
+      color: hsl(351, 98%, 49%);
+    `}
+
+  @media (max-width: 768px) {
+    bottom: 15px;
+  }
+`;
+
+const FeedbackError = styled.p`
+  font-size: 12px;
+  margin-bottom: 0;
+  color: hsl(351, 98%, 49%);
+  display: none;
+  ${(props) =>
+    props.isValid === 'false' &&
+    css`
+      display: block;
+    `}
+`;
+
+export { Input, InputGroup, Label, IconValidation, FeedbackError };
